bbbab06b9d5407a8fea2c46c542f4abc78bcb27a
manuroyce <manuroyce@outlook.com>
2022-05-05 12:32:24 +0200

    Desafio Log

---
+++ DesafioLogBManuelRC/src/main/java/com/nttdata/LogBack/App.java
@@ -0,0 +1,39 @@
+package com.nttdata.LogBack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * Clase principal.
+ * 
+ * @author Manu
+ *
+ */
+
+public class App {
+
+	/** LOGGER */
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	/**
+	 * 
+	 * Comprueba que el nombre este relleno.
+	 * 
+	 * @param args
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		// Se introduce nombre
+
+		int contador = 0;
+
+		do {
+			logger.info("Málaga \"la bombonera\"\r\n" + "flor de la costa del sol\r\n" + "tiene equipo de primera\r\n"
+					+ "en el fútbol español {}. {}", contador, App.class.getSimpleName());
+			contador++;
+		} while (contador < 100000);
+	}
+}
